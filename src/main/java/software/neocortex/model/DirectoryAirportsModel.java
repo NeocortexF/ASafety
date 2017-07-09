391be3ad34023cd72ec58838ced2256f53709b58
NeocortexF <eugeneshamkin@mail.ru>
2017-07-09 11:02:45 +0500

    directory airports model is ready

--- src/main/java/software/neocortex/model/DirectoryAirportsModel.java
+++ src/main/java/software/neocortex/model/DirectoryAirportsModel.java
@@ -1,5 +1,52 @@
 package software.neocortex.model;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name = "airports")
 public class DirectoryAirportsModel {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "airport")
+    private String airportName;
+
+    @Column(name = "code")
+    private String airportIcaoCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAirportName() {
+        return airportName;
+    }
+
+    public void setAirportName(String airportName) {
+        this.airportName = airportName;
+    }
+
+    public String getAirportIcaoCode() {
+        return airportIcaoCode;
+    }
+
+    public void setAirportIcaoCode(String airportIcaoCode) {
+        this.airportIcaoCode = airportIcaoCode;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectoryAirportsModel{" +
+                "id=" + id +
+                ", airportName='" + airportName + '\'' +
+                ", airportIcaoCode='" + airportIcaoCode + '\'' +
+                '}';
+    }
 }
