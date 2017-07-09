7c875ab1a3ed22eb9664551a427b4e4a3aa2c4ec
NeocortexF <eugeneshamkin@mail.ru>
2017-07-09 11:05:54 +0500

    classificatory directory postponed until decision of front-end  tree realisation

--- src/main/java/software/neocortex/model/DirectoryClassifierModel.java
+++ src/main/java/software/neocortex/model/DirectoryClassifierModel.java
@@ -1,4 +1,5 @@
 package software.neocortex.model;
 
+//TODO: Определиться как буду строить дерево в html. Нужны ли две таблицы: категория и подкатегория или одна
 public class DirectoryClassifierModel {
 }
