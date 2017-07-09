25d4dd69b35eae4e936804ba72c7524e5d05c021
NeocortexF <eugeneshamkin@mail.ru>
2017-07-09 11:19:21 +0500

    fleet directory model is ready

--- src/main/java/software/neocortex/model/DirectoryFleetModel.java
+++ src/main/java/software/neocortex/model/DirectoryFleetModel.java
@@ -1,4 +1,40 @@
 package software.neocortex.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "fleet")
 public class DirectoryFleetModel {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "tail")
+    private String tailNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTailNumber() {
+        return tailNumber;
+    }
+
+    public void setTailNumber(String tailNumber) {
+        this.tailNumber = tailNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectoryFleetModel{" +
+                "id=" + id +
+                ", tailNumber='" + tailNumber + '\'' +
+                '}';
+    }
 }
