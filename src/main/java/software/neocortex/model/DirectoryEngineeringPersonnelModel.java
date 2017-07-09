cb19694533d24c08a65c2d26875f9ec2bb94873e
NeocortexF <eugeneshamkin@mail.ru>
2017-07-09 11:23:20 +0500

    directory of engineering personnel model is ready

--- src/main/java/software/neocortex/model/DirectoryEngineeringPersonnelModel.java
+++ src/main/java/software/neocortex/model/DirectoryEngineeringPersonnelModel.java
@@ -1,4 +1,40 @@
 package software.neocortex.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "gopi")
 public class DirectoryEngineeringPersonnelModel {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectoryEngineeringPersonnelModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
