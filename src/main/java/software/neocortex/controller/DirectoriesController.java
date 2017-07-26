05c6701af91566941d519d8bf77226ec5efc3ea2
NeocortexF <eugeneshamkin@mail.ru>
2017-07-26 19:24:22 +0500

    added controller for list of airports

--- src/main/java/software/neocortex/controller/DirectoriesController.java
+++ src/main/java/software/neocortex/controller/DirectoriesController.java
@@ -1,9 +1,31 @@
 package software.neocortex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import software.neocortex.model.DirectoryAirportsModel;
+import software.neocortex.service.DirectoryAirportsService;
+
 
 @Controller
 public class DirectoriesController {
 
-    private
+    private DirectoryAirportsService directoryAirportsService;
+
+    @Autowired
+    @Qualifier("directoryAirportsService")
+    public void setDirectoryAirportsService(DirectoryAirportsService directoryAirportsService) {
+        this.directoryAirportsService = directoryAirportsService;
+    }
+
+    @RequestMapping(value = "airports", method = RequestMethod.GET)
+    public String listOfAirports(Model model) {
+        model.addAttribute("directoryAirportsModel", new DirectoryAirportsModel());
+        model.addAttribute("listAirports", this.directoryAirportsService.listOfAirports());
+
+        return "airports";
+    }
 }
